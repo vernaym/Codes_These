b4cfce0074bd33426b09b68bd60eef1f94fc4101
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2022-02-03 16:24:17 +0100

    Add ratio ANTILOPE/rain-gauges scatterplot

--- ANTILOPE/evaluation_ANTILOPE.py
+++ ANTILOPE/evaluation_ANTILOPE.py
@@ -127,46 +127,69 @@ def raw_scatterplot(rr_nivometeo, rr_antilope, elevations, datebegin, dateend):
 
 def elevation_scatterplot(workdf, datebegin, dateend):
 
-    fig = plt.figure(figsize=(12,9))
+    fig1, ax1 = plt.subplots(figsize=(12,9))
+    fig2, ax2 = plt.subplots(figsize=(12,9))
+
+    workdf = workdf.sort_values('elevation')
+    workdf['ratio'] = workdf['rr_antilope'] / workdf['rr_nivometeo']
+    workdf.replace([np.inf, -np.inf], np.nan, inplace=True)
+    workdf = workdf.loc[~workdf['ratio'].isna()]
 
     elevation_thresholds = [1450, 1725, 2000, 4000] # Based on the main "jumps"
     elevation_thresholds = [1530, 1739, 1967, 4000] # Based on the quantiles
     elevation_thresholds = [np.quantile(workdf['elevation'], q) for q in [0.25, 0.5, 0.75, 1]]
     colors = ['darkblue', 'cyan', 'gold', 'red']
 
-    workdf = workdf.sort_values('elevation')
+    reg = LinearRegression().fit(workdf['elevation'].to_numpy().reshape((-1, 1)), workdf['ratio'].to_numpy())
+    model = reg.predict(workdf['elevation'].to_numpy().reshape((-1,1)))
+    r2 = r2_score(workdf['elevation'].to_numpy().reshape((-1, 1)),workdf['ratio'].to_numpy())
+    ax2.plot(workdf['elevation'].to_numpy(), model, color='black', linewidth=2)
+    ax2.text(750, 0.5, f'R²={r2:.4}', fontsize=18, color='black')
 
     for i, threshold in enumerate(elevation_thresholds):
         index_names = workdf[workdf['elevation'] < threshold].index
         tmp = workdf.loc[workdf.index.isin(index_names)]
         workdf.drop(index_names, inplace = True)
-        x = tmp['rr_nivometeo'].to_numpy()
-        y = tmp['rr_antilope'].to_numpy()
-        if i == 0:
-            label = '< {0:d} m'.format(int(threshold)) 
-        elif i == 3:
-            label = '> {0:d} m'.format(int(elevation_thresholds[i-1]))
-        else:
-            label = '[{0:d} m - {1:d} m]'.format(int(elevation_thresholds[i-1]), int(elevation_thresholds[i]))
-        sc = plt.scatter(x, y, marker='D', s=10, c=colors[i], label=label)
-        # Regression linéaire
-        reg = LinearRegression().fit(x.reshape((-1, 1)), y)
-        y = reg.predict(x.reshape((-1,1)))
-        plt.plot(x, y, color=colors[i], linewidth=1)
+        
+        def plot(x, y, ax, regression=True):
+            if regression:
+                reg = LinearRegression().fit(x.reshape((-1, 1)), y)
+                model = reg.predict(x.reshape((-1,1)))
+                r2 = r2_score(x.reshape((-1, 1)), y)
+                det = " R²={0:.4f}".format(r2)
+            else:
+                det = ""
+            if i == 0:
+                label = ','.join(['< {0:d} m'.format(int(threshold)), det])
+            elif i == 3:
+                label = ','.join(['> {0:d} m'.format(int(elevation_thresholds[i-1])), det])
+            else:
+                label = ','.join(['[{0:d} m - {1:d} m]'.format(int(elevation_thresholds[i-1]), int(elevation_thresholds[i])), det])
+            ax.scatter(x, y, marker='D', s=10, c=colors[i], label=label)
+            if regression:
+                ax.plot(x, model, color=colors[i], linewidth=1)
 
-    plt.legend()
-    xpoints = ypoints = plt.xlim()
-    plt.grid(visible=True, linestyle=':', linewidth=0.5)
-    plt.xlim(xmin=0)
-    plt.ylim(ymin=0)
-    plt.plot(xpoints, ypoints, linestyle='--', color='grey', lw=1, scalex=False, scaley=False)
-    plt.ylabel('Total ANTILOPE precipitation estimate (mm)', fontsize=12)
-    plt.xlabel('Total rain-gauges observed precipitation (mm)', fontsize=12)
+        plot(tmp['rr_nivometeo'].to_numpy(), tmp['rr_antilope'].to_numpy(), ax1, regression=True)
+        plot(tmp['elevation'].to_numpy(), tmp['ratio'].to_numpy(), ax2, regression=False)
 
+    def layout(ax):
+        ax.legend(fontsize=14)
+        xpoints = ypoints = ax.get_xlim()
+        ax.grid(visible=True, linestyle=':', linewidth=0.5)
+        ax.plot(xpoints, ypoints, linestyle='--', color='grey', lw=1, scalex=False, scaley=False)
 
+    ax1.set_xlim(left=0)
+    ax1.set_ylim(bottom=0)
+    layout(ax1)
+    layout(ax2)
+    ax1.set_ylabel('Total ANTILOPE precipitation estimate (mm)', fontsize=12)
+    ax1.set_xlabel('Total rain-gauges observed precipitation (mm)', fontsize=12)
+    ax2.set_ylabel('Total ANTILOPE/rain-gauges ratios', fontsize=12)
+    ax2.set_xlabel('Rain-gauges elevations (m)', fontsize=12)
     #plt.tight_layout()
     #fig.savefig('scatterplot_{0:s}_{1:s}.pdf'.format(datebegin.strftime('%Y%m%d'), dateend.strftime('%Y%m%d')), bbox_inches='tight', format='pdf')
-    fig.savefig('elevation_scatterplot_{0:s}_{1:s}.png'.format(datebegin.strftime('%Y%m%d'), dateend.strftime('%Y%m%d')), bbox_inches='tight', format='png')
+    fig1.savefig('scatterplot_by_elevation_{0:s}_{1:s}.png'.format(datebegin.strftime('%Y%m%d'), dateend.strftime('%Y%m%d')), bbox_inches='tight', format='png')
+    fig2.savefig('ratio_scatterplot_by_elevation_{0:s}_{1:s}.png'.format(datebegin.strftime('%Y%m%d'), dateend.strftime('%Y%m%d')), bbox_inches='tight', format='png')
 
 if __name__ == "__main__":
     args = parse_command_line()
@@ -195,6 +218,7 @@ if __name__ == "__main__":
     #raw_scatterplot(rr_nivometeo.to_numpy(), rr_antilope.to_numpy(), elevations.to_numpy(), args.datebegin, args.dateend)
     workdict = {'elevation':elevations, 'rr_nivometeo':rr_nivometeo, 'rr_antilope':rr_antilope, 'nb_values':nb_values}
     workdf   = pd.DataFrame(workdict)
+    #workdf = workdf.loc[~workdf['rr_nivometeo'].isnull()].loc[~workdf['rr_antilope'].isnull()] 
     elevation_scatterplot(workdf, args.datebegin, args.dateend)
 
 
