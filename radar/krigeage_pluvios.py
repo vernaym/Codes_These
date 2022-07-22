1d09d5c9643bdfc71c410a5f67c65631cde8edd8
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2022-07-22 10:54:30 +0200

    Add 3D maps of cumulated ANTILOPE and KRIGING precipitation

--- radar/krigeage_pluvios.py
+++ radar/krigeage_pluvios.py
@@ -99,19 +99,19 @@ if __name__ == "__main__":
 
     if args.data == 'nivometeo':
         reference = read_ref_coords('alp')
-        fic = 'obs_nivometeo_hourly_RR_20161101_20220419.csv'
+        fic = 'obs_nivometeo.csv'
         pluvios = pd.read_csv(fic, sep=';', parse_dates=['H.dat'], dtype={'Q.num_poste':int, 'poste_nivo.nom_usuel':str, 'poste_nivo.massif_nivo':int,
             'poste_nivo.lat_dg':float, 'poste_nivo.lon_dg':float, 'poste_nivo.alti':int, 'rr':float, 'hist_reseau_poste.reseau_poste':int}, na_values=['--'])
         pluvios.rename(columns={'H.dat':'dat', 'H.num_poste':'num_poste', 'poste_nivo.lat_dg':'lat', 'poste_nivo.lon_dg':'lon', 'H.rr1':'rr',
             'poste_nivo.alti':'alti', 'hist_reseau_poste.reseau_poste':'reseau_poste'}, inplace=True)
     elif args.data == 'all':
         reference = dict()
-        fic1 = f'obs_nivometeo_{args.datebegin.ymdh}_{args.dateend.ymdh}.csv'
-        pluvios1 = pd.read_csv(fic1, sep=';', parse_dates=['H.dat'], dtype={'Q.num_poste':int, 'poste_nivo.nom_usuel':str, 'poste_nivo.massif_nivo':int,
+        fic1 = 'obs_nivometeo.csv'
+        pluvios1 = pd.read_csv(fic1, sep=';', parse_dates=['Q.dat'], dtype={'Q.num_poste':int, 'poste_nivo.nom_usuel':str, 'poste_nivo.massif_nivo':int,
             'poste_nivo.lat_dg':float, 'poste_nivo.lon_dg':float, 'poste_nivo.alti':int, 'rr':float, 'hist_reseau_poste.reseau_poste':int}, na_values=['--'])
         pluvios1.rename(columns={'H.dat':'dat', 'H.num_poste':'num_poste', 'poste_nivo.lat_dg':'lat', 'poste_nivo.lon_dg':'lon', 'H.rr1':'rr',
             'poste_nivo.alti':'alti', 'hist_reseau_poste.reseau_poste':'reseau_poste'}, inplace=True)
-        fic2 = ".csv"
+        fic2 = "autres_obs.csv"
         pluvios2 = pd.read_csv(fic2, sep=';', parse_dates=['dat'], dtype={'num_poste':int, 'poste':str, 'lat':float, 'lon':float, 'alti':int, 'rr':float, 'reseau_poste':int}, na_values=['--'])
         pluvios = pd.concat([pluvios1, pluvios2], ignore_index=True, sort=True)
     else:
@@ -160,7 +160,8 @@ if __name__ == "__main__":
             # plt.imshow(rr24, interpolation='none', cmap='jet')
             # plt.show()
             fig = cartopy.Map_alpes()
-            cf = plt.contourf(lon, lat, rr24.data, levels=100, cmap='YlGnBu')
+            #cf = plt.contourf(lon, lat, rr24.data, levels=100, cmap='YlGnBu')
+            cf = plt.contourf(lon, lat, rr24.data, cmap='YlGnBu')
             plt.colorbar(cf, label='24h precipitation (mm)')
             fig.init_massifs()
             fig.addpoints(x.values, y.values, labels=np.around(rr, 1))
--- radar/plot_radar_cumuls.py
+++ radar/plot_radar_cumuls.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # Auteur: Matthieu Vernay
 # Date : 02/02/2022
- 
-import sys
+
+import os,sys
 import pandas as pd
 import xarray as xr
 import numpy as np
@@ -15,17 +15,34 @@ from mpl_toolkits.mplot3d import Axes3D  # F401 unused import --> to ignore !
 #import hvplot.xarray
 from pyproj import Proj, transform
 
+# This script plot accumulated precipiation over the Grandes-Rousses domain (can
+# be easily adapted to plot over different domain or the whole input domain)
+# It interpolates a DEM grid on the grided precipitation data to plot 3D
+# precipitation fields
 
 print('USAGE : plot_radar_cumuls.py inputfile')
+datadir = '/home/vernaym/These/DATA'
+savedir = '/home/vernaym/These/figures'
 
 filename = sys.argv[1]
 
+datebegin = filename.split('.')[0].split('_')[-2]
+dateend   = filename.split('.')[0].split('_')[-1]
+
+if not os.path.isfile(filename):
+    print(f'WARNING : no such file or directory {filename}')
+    print(f'lookink for the file under {datadir}')
+    filename = os.path.join(datadir, filename)
+    if not os.path.isfile(filename):
+        print(f'ERROR : no such file or directory {filename}')
+        sys.exit(1)
+
 # Domaine des Grandes Rousses
 extract_dom = dict(
-        latmax = 45.240,
-        latmin = 44.990,
-        lonmin = 6.010,
-        lonmax = 6.490,
+    latmax = 45.240,
+    latmin = 44.990,
+    lonmin = 6.010,
+    lonmax = 6.490,
 )
 
 norm = plt.Normalize()
@@ -65,19 +82,17 @@ else:
     ds = xr.open_dataset(filename)
     #mnt = xr.open_dataset("scriptMNTLouis.nc")  # MNT uniquement sur les grandes Rousses
     mnt = xr.open_dataset('/home/vernaym/QGIS/MNT/DEM_ALPES_WGS84_250m_bilinear.nc')  # Pour tracer sur toutes les Alpes
-    if filename.startswith('ANTILOPE'):
+    if 'ANTILOPE' in filename:
         product = 'ANTILOPE'
         #tmp = ds.interp(X=mnt.lon, Y=mnt.lat, method='nearest') # Pour interpoller la grille ANTILOPE sur le MNT
         tmp = mnt.interp(lon=ds.longitude, lat=ds.latitude, method='nearest')  # Pour interpoller le MNT sur la grille ANTILOPE
-    else:
+    elif 'krigeage' in filename:
         product = 'KRIGING'
         #tmp = ds.interp(X=mnt.x, Y=mnt.y, method='nearest') # Pour interpoller la grille ANTILOPE sur le MNT
         tmp = mnt.interp(lon=ds.longitude, lat=ds.latitude, method='nearest')  # Pour interpoller le MNT sur la grille ANTILOPE
 
-
     tmp['Z'] = tmp['Band1']
     tmp['Z'].values = np.nan_to_num(tmp['Z'].values)
-
 
     latmin = extract_dom['latmin']
     lonmin = extract_dom['lonmin']
@@ -86,17 +101,16 @@ else:
 
     # On selectionne le sous domaine d'intéret avant de plotter...
     tmp = tmp.where((tmp.lon>=lonmin) & (tmp.lon<=lonmax) & (tmp.lat>=latmin) & (tmp.lat<=latmax), drop=True)
-    X = tmp['longitude'].values
-    Y = tmp['latitude'].values
+    X = tmp['lon'].values
+    Y = tmp['lat'].values
     Z = tmp['Z']
-
 
     # define pixel colors
     radar = ds.where((ds.longitude>=lonmin) & (ds.longitude<=lonmax) & (ds.latitude>=latmin) & (ds.latitude<=latmax), drop=True).rr_cumul
     colors = plt.cm.coolwarm(norm(np.nan_to_num(radar.values)))
 
-
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.view_init(elev=60., azim=135)  # Set point of view
 surf = ax.plot_surface(X=X, Y=Y, Z=Z, linewidth=0, antialiased=False, facecolors=colors)
 ax.xaxis.pane.fill = False
 ax.xaxis.pane.set_edgecolor('white')
@@ -112,6 +126,7 @@ ax.set_zlabel('Elevation (m)')
 ax.set_zlim(0., 3500.)
 #fig.colorbar(surf, shrink=0.5, aspect=5)
 #fig.colorbar(colorbar=colors, shrink=0.5, aspect=5)
-fig.colorbar(cm.ScalarMappable(norm=norm, cmap=plt.cm.coolwarm), ax=ax, shrink=0.75, aspect=8, label=f'{product} cumulated precipitation \n between 2018113006 and 2019043006 (mm)')
-plt.show()
-#fig.save("CUMULS_PANTHERE_3D.svg", format='svg')
+fig.colorbar(cm.ScalarMappable(norm=norm, cmap=plt.cm.coolwarm), ax=ax, shrink=0.75, aspect=8, label=f'{product} cumulated precipitation \n between {datebegin} and {dateend} (mm)')
+#plt.show()
+plt.savefig(f'{savedir}/CUMUL3D_{product}_{datebegin}_{dateend}.pdf', format='pdf')
+
