b1fe7ecd11c78db612e5aaae86bd5ecd49150dcf
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2022-10-26 09:51:34 +0200

    Evaluation of a set of experiment for comparison

--- ParticleFilter/evaluation.py
+++ ParticleFilter/evaluation.py
@@ -4,6 +4,7 @@
 # Date : 02/02/2022
 
 import os, sys
+import glob
 from datetime import datetime,timedelta
 import numpy as np
 import xarray as xr
@@ -39,14 +40,15 @@ coords = dict(
 
 domain = 'GrandesRousses'
 
-savedir = "/home/vernaym/workdir/ASSIMILATION/XP00"
+savedir = "/home/vernaym/These/figures"
 
 class Evaluation(object):
 
-    def __init__(self, ensemble):
-        self.ensemble = ensemble  # DataArray(lat,lon,time,member)
-        self.Ne = len(self.ensemble.member)
-        self.read_nivometeo_obs()  # Read observation --> self.obs
+    def __init__(self):
+        #self.data = xr.Dataset()
+#        self.ensemble = ensemble  # DataArray(lat,lon,time,member)
+        self.Ne = 16  # TODO : à definir dynamiquement
+        self.scores = None
 
     def ensemble_attributes(self):
         disp = self.dispersion()
@@ -61,6 +63,15 @@ class Evaluation(object):
     def median_error(self, simu, obs):
         return simu.median() - obs
 
+    def bias(self, simu, obs, **kw):
+
+        if np.shape(simu) == np.shape(obs):  # "Simulation" déterministe
+            bias = simu - obs
+        else:  # Simulation s'ensmble
+            bias = self.mean_error(simu, obs)
+
+        return np.nanmean(bias)
+
     def dispersion(self):
         """
         spread over all dates (and pixels ?)
@@ -70,21 +81,28 @@ class Evaluation(object):
 
         return disp
 
-    def rmse(self, simu, obs, num_poste):
-        rmse = np.sqrt(np.mean(np.square(simu.mean() - obs)))
-        #print(f'RMSE for station {num_poste} :',rmse)
-        print(f'RMSE=',rmse)
+    def rmse(self, simu, obs, **kw):
+
+        if np.shape(simu) == np.shape(obs):  # "Simulation" déterministe
+            rmse = np.sqrt(np.nanmean(simu-obs))
+        else:  # Simulation d'ensemble
+            rmse = np.sqrt(np.nanmean(np.square(simu.mean() - obs)))
         return rmse
 
     def brier_skill_score(self, simu, ref, obs, threshold):
         """  BSS = 1 - BS / BSref  """
         return 1 - self.brier_score(simu, obs, threshold=threshold) / self.brier_score(ref, obs, threshold=threshold)
 
-    def brier_score(self, simu, obs, threshold):
-        pens  = np.count_nonzero(simu>=threshold, axis=0) / self.Ne
+    def brier(self, simu, obs, threshold=1):
+
+        if np.shape(simu) == np.shape(obs):  # "Simulation" déterministe
+            psimu = np.where(simu>=threshold, 1, 0)
+        else:  # Simulation d'ensemble
+            psimu  = np.count_nonzero(simu>=threshold, axis=1) / self.Ne
         fobs   = np.where(obs>=threshold, 1, 0)
-        brier = np.mean((pens-fobs)**2)
-        print('Brier=',brier)
+        brier = np.nanmean((psimu-fobs)**2)
+        #print('Brier=',brier)
+
         return brier
 
     def ROC(self, simu, obs, threshold):
@@ -102,10 +120,12 @@ class Evaluation(object):
         succes_rate = list()
         false_alarm = list()
         for seuil in range(1, self.Ne+1):
+            # Pour un dépassement de seuil :
 #                a = np.count_nonzero(np.where((obs>=threshold) & (np.count_nonzero(simu>=threshold, axis=0)>=seuil)))
 #                b = np.count_nonzero(np.where((obs<threshold) & (np.count_nonzero(simu>=threshold, axis=0)>=seuil)))
 #                c = np.count_nonzero(np.where((obs>=threshold) & (np.count_nonzero(simu>=threshold, axis=0)<seuil)))
 #                d = np.count_nonzero(np.where((obs<threshold) & (np.count_nonzero(simu>=threshold, axis=0)<seuil)))
+            # Pour un intervalle :
             a = np.count_nonzero(np.where((obs>=1) & (obs<5) & (np.count_nonzero((simu>=1) & (simu<5), axis=0)>=seuil)))
             b = np.count_nonzero(np.where(((obs<1) | (obs>=5)) & (np.count_nonzero((simu>=1) & (simu<5), axis=0)>=seuil)))
             c = np.count_nonzero(np.where((obs>=1) & (obs<5) & (np.count_nonzero((simu>=1) & (simu<5), axis=0)<seuil)))
@@ -121,8 +141,6 @@ class Evaluation(object):
         plt.ylim([0, 1])
         plt.legend()
         plt.show()
-
-
 
     def brier_decomposition(self, simu, obs, threshold, nb_cat=17):
 
@@ -162,55 +180,199 @@ class Evaluation(object):
         return uncertainty
 
     def read_nivometeo_obs(self):
-        nivometeo = pd.read_csv(os.path.join(datadir, 'obs_nivometeo_daily_RR_20211201_20220430.csv'), sep=';', parse_dates=['Q.dat'],
-            dtype={'Q.num_poste':int, 'poste_nivo.nom_usuel':str, 'poste_nivo.alti':int, 'poste_nivo.lat_dg':float, 'poste_nivo.lon_dg':float, 'poste_nivo.massif_nivo':int, 'Q.rr':float})
+#        nivometeo = pd.read_csv(os.path.join(datadir, 'obs_nivometeo_daily_RR_20211201_20220430.csv'), sep=';', parse_dates=['Q.dat'],
+#            dtype={'Q.num_poste':int, 'poste_nivo.nom_usuel':str, 'poste_nivo.alti':int, 'poste_nivo.lat_dg':float, 'poste_nivo.lon_dg':float, 'poste_nivo.massif_nivo':int, 'Q.rr':float},)
+#            rename={'Q.date':'date', 'Q.num_poste':'num_poste', 'poste_nivo.nom_usuel':'nom', 'poste_nivo.alti':'alti', 'poste_nivo.lat_dg':'lat', 'poste_nivo.lon_dg':'lon', 'Q.rr':'obs'})
+
+            #Q.dat;Q.num_poste;poste_nivo.nom_usuel;poste_nivo.alti;poste_nivo.lat_dg;poste_nivo.lon_dg;poste_nivo.massif_nivo;Q.rr;hist_reseau_poste.reseau_poste
+
+        nivometeo = pd.read_csv(os.path.join(datadir, 'obs_nivometeo_daily_RR_20211201_20220430.csv'), sep=';', parse_dates=['date'], header=0,
+                names=['date', 'num_poste', 'nom', 'alti', 'lat', 'lon', 'massif', 'obs', 'unused'],
+                usecols=['date', 'num_poste', 'nom', 'alti', 'lat', 'lon', 'obs'],
+                dtype={'num_poste':int, 'nom':str, 'alti':int, 'lat':float, 'lon':float, 'obs':float},
+            )
 
         latmax, latmin, lonmin, lonmax = np.array(coords[domain]).astype(float)/1000.
-        self.obs = nivometeo.loc[(nivometeo['poste_nivo.lat_dg']>=latmin) & (nivometeo['poste_nivo.lat_dg']<=latmax) & (nivometeo['poste_nivo.lon_dg']>=lonmin) & (nivometeo['poste_nivo.lon_dg']<=lonmax)]
+        nivometeo = nivometeo.loc[(nivometeo['lat']>=latmin) & (nivometeo['lat']<=latmax) & (nivometeo['lon']>=lonmin) & (nivometeo['lon']<=lonmax)]  # Select area
+        nivometeo.date = nivometeo.date + pd.Timedelta("1d6h")   #BDClim extraction for date ymd is the observation from ymd6h to ym(d+1)6h
+        #nivometeo.groupby('num_poste')['nom', 'lat', 'lon', 'alti'].agg(set)
+        #nivometeo = nivometeo.set_index(['num_poste', 'lat', 'lon', 'nom', 'alti', 'date'])  # Utilité de passer en index ?
+        nivometeo.set_index(['num_poste','date'], inplace=True)
 
-    def read_nivometeo_coords(self, domain):
-        metadata = pd.read_csv(os.path.join(datadir, 'postes_nivometeo.csv'), sep=';')
-        latmax, latmin, lonmin, lonmax = np.array(coords[domain]).astype(float)/1000.
-        subdata = metadata[(metadata['poste_nivo.lat_dg']>=latmin) & (metadata['poste_nivo.lat_dg']<=latmax) & (metadata['poste_nivo.lon_dg']>=lonmin) & (metadata['poste_nivo.lon_dg']<=lonmax)]
-        return dict(zip(np.array(subdata['poste_nivo.num_poste']), zip(np.array(subdata['poste_nivo.lat_dg']), np.array(subdata['poste_nivo.lon_dg']))))
+        return nivometeo.to_xarray()
+
+#    def read_nivometeo_coords(self, domain):
+#        metadata = pd.read_csv(os.path.join(datadir, 'postes_nivometeo.csv'), sep=';')
+#        latmax, latmin, lonmin, lonmax = np.array(coords[domain]).astype(float)/1000.
+#        subdata = metadata[(metadata['poste_nivo.lat_dg']>=latmin) & (metadata['poste_nivo.lat_dg']<=latmax) & (metadata['poste_nivo.lon_dg']>=lonmin) & (metadata['poste_nivo.lon_dg']<=lonmax)]
+#        return dict(zip(np.array(subdata['poste_nivo.num_poste']), zip(np.array(subdata['poste_nivo.lat_dg']), np.array(subdata['poste_nivo.lon_dg']))))
+#
+    def read_antilope(self):
+        filename = 'ANTILOPEQ_2021073106_2022070106_GrandesRousses.nc'
+        antilope = xr.open_dataset(os.path.join(datadir, filename))
+        return antilope
+
+    def read_raw_ensemble(self):
+        filenames = [os.path.join(datadir, f'aspearome_{mb:03d}_2021073106_2022070106_GrandesRousses_daily.nc') for mb in range(1,17)]
+        raw = xr.open_mfdataset(filenames, combine='nested', concat_dim='member').compute()
+        raw['member']=np.arange(1,17)
+        raw = raw.transpose('lat', 'lon', 'time', 'member')  # transpose data to put dimension in the same order as assimilated fields
+
+        return raw
+
+    def read_simu(self, filename):
+        if not os.path.exists(filename):
+            print(f'WARNING : file {filename} does not exist, looking for it under {datadir}')
+            filename = os.path.join(datadir, filename)
+
+        if os.path.exists(filename):
+            simulation =  xr.open_dataset(os.path.join(datadir, filename))
+        else:
+            print(f'ERROR : file {filename} does not exist')
+            sys.exit(1)
+
+        if 'hourly' in filename:
+            # Convert hourly precipitation into 24h precipitation between 6h J-1 and 6h J
+            # Problem : the xarray tools to do that allows only accumulations between
+            # 0h and 23h.
+            # solution : shift time serie by 7h, compute 24h accumulations and
+            # shift back !
+            print(simulation.time.data[23])
+            simulation['time'] = simulation.time-np.timedelta64(7, 'h')
+            toto=simulation.loc[{'lat':44.99, 'lon':6.01, 'member':1}].rr.data
+            simulation = simulation.resample(time='D').sum(dim='time')  # !!! VERY SLOW !!!
+            tata=simulation.loc[{'lat':44.99, 'lon':6.01, 'member':1}].rr.data
+            print(toto[:24])
+            print(np.sum(toto[:24]), tata[0])
+            simulation['time'] = simulation.time+np.timedelta64(30, 'h')
+            print(simulation.time.data[0])
+            #import pdb
+            #pdb.set_trace()
+
+        return simulation
 
     def evaluate(self):
+
+#        if os.path.exists(os.path.join(datadir, 'scores.nc')):
+#            self.scores = xr.open_dataset(os.path.join(datadir, 'scores.nc'))
+#            return
 
         def nearest(array, value):
             # Find element of "array" the closer to "value"
             return float(array[np.abs(array - value).argmin()].data)
-
         threshold = 1
 
         # To Extract specific values where evaluation data (obs nivometeo) is available
         pos = 1
 
         # TODO : gerer les données avec une DataFrame ou un DataSet
-        dates = dict()
-        simu  = dict()
-        obs   = dict()
-        brier = dict()
-        for num_poste, (lat, lon) in self.read_nivometeo_coords(domain).items():
-            nearest_lat = nearest(self.ensemble.lat, lat)
-            nearest_lon = nearest(self.ensemble.lon, lon)
-            tmp  = self.obs.loc[self.obs["Q.num_poste"]==num_poste]
-            if len(tmp)>0:
-                print(f'Poste {num_poste} ({len(tmp)} data)')
-                dates[num_poste] = np.array(tmp["Q.dat"] + timedelta(hours=30))  # Date yyyymmdd is the cumul between yyyymmdd06 and yyyymm(d+1)06
-                obs[num_poste]  = np.array(tmp['Q.rr'])
-                # Select corresponding simulations
-                simu[num_poste] = np.transpose(self.ensemble.sel({'lat':nearest_lat, 'lon':nearest_lon}).loc[{'time':dates[num_poste]}].rr.data)
-                self.rmse(simu[num_poste], obs[num_poste], num_poste)
-                fiability, resolution, uncertainty = self.brier_decomposition(simu[num_poste], obs[num_poste], threshold)
-                print('BSfiab+BSres+BSunc=',fiability-resolution+uncertainty)
-                brier       = self.brier_score(simu[num_poste], obs[num_poste], threshold)
-                #self.temporal_plot(time, simu, obs, num_poste)
-                #plt.violinplot(self.mean_error(simu, obs), positions=[pos])
-                pos = pos + 1
-        self.ROC(np.concatenate([array for array in simu.values()], axis=1), np.concatenate([array for array in obs.values()]), threshold)
-        brier_global = self.brier_score(np.concatenate([array for array in simu.values()], axis=1), np.concatenate([array for array in obs.values()]), threshold)
-        #plt.show()
-        return tmp
+#        simu  = dict()
+#        obs   = dict()
+#        brier = dict()
+#
+#        dates  = list()
+#        postes = list()
+#        obs    = list()
+#        data   = dict()
+#        liste_poste = self.read_nivometeo_coords(domain).keys()
+#        latmax, latmin, lonmin, lonmax = np.array(coords[domain]).astype(float)/1000.
+
+        self.data = self.read_nivometeo_obs()  # Read observation --> self.obs
+        # Remove time dimension from metadata :
+        self.data['lon']=np.max(self.data.lon, axis=1)
+        self.data['lat']=np.max(self.data.lat, axis=1)
+        self.data['alti']=np.max(self.data.alti, axis=1)
+        self.data['nom']=np.max(self.data.nom, axis=1)
+#        data = self.obs.loc[self.obs["Q.num_poste"].isin(liste_poste)]  # TODO a adapter
+#        self.stations = self.data[['num_poste', 'nom', 'lat', 'lon', 'alti']].drop_duplicates()
+
+        antilope = self.read_antilope()
+
+        raw = self.read_raw_ensemble()
+        self.data['member'] = np.arange(1,17)
+
+        data = dict(antilope=list(), raw=list())
+        experiments = dict(
+#                LD0      = 'Assimilation_locale_2021080106_2022070106.nc',
+                LH0      = 'Assimilation_locale_2021080106_2022070106_hourly.nc',
+#                LDM      = 'Assimilation_locale_2021073106_2022070106_avec_masque.nc',
+            )
+
+
+        scores_list = ['rmse', 'bias', 'brier']
+        scores = dict()
+        dates = self.data.date
+        for num_poste in self.data.num_poste.data:
+#            num_poste = row['num_poste']
+#            lat       = row['lat']
+#            lon       = row['lon']
+#            dates     = self.data[self.data['num_poste']==num_poste]['date'].values
+            lat = self.data.loc[{'num_poste':num_poste}].lat
+            lon = self.data.loc[{'num_poste':num_poste}].lon
+            obs = self.data.loc[{'num_poste':num_poste}].obs.data
+            data['antilope'].append(antilope.sel({'lat':nearest(antilope.lat, lat), 'lon':nearest(antilope.lon, lon)}).loc[{'time':dates}].rr.data)
+            data['raw'].append(raw.sel({'lat':nearest(raw.lat, lat), 'lon':nearest(raw.lon, lon)}).loc[{'time':dates}].rr.data)
+            for xpid,filename in experiments.items():
+                if xpid not in data.keys():
+                    data[xpid] = list()
+                simu = self.read_simu(os.path.join(datadir, filename))
+                data[xpid].append(simu.sel({'lat':nearest(simu.lat, lat), 'lon':nearest(simu.lon, lon)}).loc[{'time':dates}].rr.data)
+
+            for product in data.keys():
+                if product not in scores.keys():
+                    scores[product] = {score:list() for score in scores_list}
+                for score_name, score in scores[product].items():
+                    score.append(getattr(self, score_name)(data[product][0], obs, threshold=threshold))
+
+        self.data['antilope'] = (('num_poste', 'date'), data['antilope'])
+        self.data['raw'] = (('num_poste', 'date', 'member'), data['raw'])
+        for xpid in experiments.keys():
+            self.data[xpid] = (('num_poste', 'date', 'member'), data[xpid])
+
+        tmp = {"score":{"dims": ("score"), "data":scores_list}, "num_poste":{"dims": ("num_poste"), "data":self.data.num_poste.data}}
+        tmp.update({key:{"dims": ("score", "num_poste"), "data":[value[score] for score in scores_list]} for key,value in scores.items()})
+        self.scores = xr.Dataset.from_dict(tmp)
+        self.scores.to_netcdf(os.path.join(datadir, 'scores.nc'))
+
+#        self.rmse(simu[num_poste], obs[num_poste], num_poste)
+#        fiability, resolution, uncertainty = self.brier_decomposition(simu[num_poste], obs[num_poste], threshold)
+#        print('BSfiab+BSres+BSunc=',fiability-resolution+uncertainty)
+#        brier       = self.brier_score(simu[num_poste], obs[num_poste], threshold)
+#                nearest_lat = nearest(self.ensemble.lat, lat)
+#                nearest_lon = nearest(self.ensemble.lon, lon)
+#                print(f'Poste {num_poste} ({len(tmp)} data)')
+#                dates[num_poste] = np.array(tmp["Q.dat"] + timedelta(hours=30))  # Date yyyymmdd is the cumul between yyyymmdd06 and yyyymm(d+1)06
+#                obs[num_poste]  = np.array(tmp['Q.rr'])
+#                # Select corresponding simulations
+#                simu[num_poste] = np.transpose(self.ensemble.sel({'lat':nearest_lat, 'lon':nearest_lon}).loc[{'time':dates[num_poste]}].rr.data)
+#                self.rmse(simu[num_poste], obs[num_poste], num_poste)
+#                fiability, resolution, uncertainty = self.brier_decomposition(simu[num_poste], obs[num_poste], threshold)
+#                print('BSfiab+BSres+BSunc=',fiability-resolution+uncertainty)
+#                brier       = self.brier_score(simu[num_poste], obs[num_poste], threshold)
+#                #self.temporal_plot(time, simu, obs, num_poste)
+#                #plt.violinplot(self.mean_error(simu, obs), positions=[pos])
+#                pos = pos + 1
+#        import pdb
+#        pdb.set_trace()
+#        xr.Dataset(data_vars=dict(obs=(["num_poste","time"],obs)),coords=dict(num_poste=(["num_poste"],postes),time=(["num_poste", "time"],dates)),attrs=dict(description="Evaluation dataset"),)
+#        self.ROC(np.concatenate([array for array in simu.values()], axis=1), np.concatenate([array for array in obs.values()]), threshold)
+#        brier_global = self.brier_score(np.concatenate([array for array in simu.values()], axis=1), np.concatenate([array for array in obs.values()]), threshold)
+#        #plt.show()
+#        return tmp
+
+    def plot_scores(self):
+        if self.scores is None:
+            self.evaluate()
+        for score in self.scores.score.data:
+            fig,ax = plt.subplots()
+            pos = 1
+            products = [var for var in self.scores.data_vars]
+            for product in products:
+                x = self.scores.loc[{'score':score}][product].data
+                plt.violinplot(x[~np.isnan(x)], showmeans=True, positions=[pos])
+                pos += 1
+            ax.set_xticklabels([''] + products)
+            fig.savefig(f'{savedir}/{score}.pdf', formatout='pdf',  bbox_inches='tight')
 
     def temporal_plot(self, time, simu, obs, num_poste):
 
@@ -224,10 +386,8 @@ class Evaluation(object):
     def eval_simu(self):
         pass
 
-
 if __name__ == "__main__":
 
-    data = xr.open_dataset(os.path.join(datadir, 'Assimilation_locale_2021080106_2022070106.nc'))
-    evaluation = Evaluation(data)
-    evaluation.ensemble_attributes()
-    evaluation.evaluate()
+    evaluation = Evaluation()
+    #evaluation.ensemble_attributes()
+    evaluation.plot_scores()
