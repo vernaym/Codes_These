f69002dffda68e071cb75c375211c20965dbbcf8
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2023-08-22 13:59:45 +0200

    Add script to evaluate the ratio/error estimation method

---
+++ radar/evaluation_methode_debiaisage.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Auteur: Matthieu Vernay
+# Date : 02/02/2022
+
+import os, sys
+import glob
+import time
+from datetime import datetime,timedelta
+import numpy as np
+import xarray as xr
+import pandas as pd
+from scipy.stats import rankdata
+import CRPS.CRPS as pscore
+
+from These.scripts import scores
+
+import argparse
+
+import matplotlib as mpl
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+datadir = '/home/vernaym/These/DATA'
+workdir = '/home/vernaym/workdir/ASSIMILATION/mask/evaluation'
+
+def read_nivometeo_obs():
+#        nivometeo = pd.read_csv(os.path.join(datadir, 'obs_nivometeo_daily_RR_20211201_20220430.csv'), sep=';', parse_dates=['Q.dat'],
+#            dtype={'Q.num_poste':int, 'poste_nivo.nom_usuel':str, 'poste_nivo.alti':int, 'poste_nivo.lat_dg':float, 'poste_nivo.lon_dg':float, 'poste_nivo.massif_nivo':int, 'Q.rr':float},)
+#            rename={'Q.date':'date', 'Q.num_poste':'num_poste', 'poste_nivo.nom_usuel':'nom', 'poste_nivo.alti':'alti', 'poste_nivo.lat_dg':'lat', 'poste_nivo.lon_dg':'lon', 'Q.rr':'obs'})
+
+        #Q.dat;Q.num_poste;poste_nivo.nom_usuel;poste_nivo.alti;poste_nivo.lat_dg;poste_nivo.lon_dg;poste_nivo.massif_nivo;Q.rr;hist_reseau_poste.reseau_poste
+
+    nivometeo = pd.read_csv(os.path.join(datadir, 'obs_nivometeo_daily_RR_20211201_20220430.csv'), sep=';', parse_dates=['date'], header=0,
+            names=['date', 'num_poste', 'nom', 'alti', 'lat', 'lon', 'massif', 'obs', 'unused'],
+            usecols=['date', 'num_poste', 'nom', 'alti', 'lat', 'lon', 'obs'],
+            dtype={'num_poste':int, 'nom':str, 'alti':int, 'lat':float, 'lon':float, 'obs':float},
+        )
+
+    coords = ['46450', '44100', '5400', '7200']  # Alp domain
+    latmax, latmin, lonmin, lonmax = np.array(coords).astype(float)/1000.
+    nivometeo = nivometeo.loc[(nivometeo['lat']>=latmin) & (nivometeo['lat']<=latmax) & (nivometeo['lon']>=lonmin) & (nivometeo['lon']<=lonmax)]  # Select area
+    #nivometeo = nivometeo[nivometeo['num_poste'].isin(indep)]  # Select evaluation stations
+    nivometeo.date = nivometeo.date + pd.Timedelta("1d6h")   #BDClim extraction for date ymd is the observation from ymd6h to ym(d+1)6h
+    #nivometeo.groupby('num_poste')['nom', 'lat', 'lon', 'alti'].agg(set)
+    #nivometeo = nivometeo.set_index(['num_poste', 'lat', 'lon', 'nom', 'alti', 'date'])  # Utilité de passer en index ?
+    nivometeo.set_index(['num_poste','date'], inplace=True)
+
+    return nivometeo.to_xarray()
+
+def read_obs_clim():
+
+    obs = pd.read_csv(os.path.join(datadir, "obs_quotidienne_clim_RR.data"), sep=';', parse_dates=['date'], header=0,
+            names = ['num_poste', 'lat', 'lon', 'alti', 'nom', 'reseau_poste', 'date', 'obs'],
+            usecols=['num_poste', 'lat', 'lon', 'alti', 'nom', 'date', 'obs'],
+            dtype={'num_poste':int, 'nom':str, 'alti':int, 'lat':float, 'lon':float, 'obs':float},
+            )
+    latmax, latmin, lonmin, lonmax = np.array(coords[domain]).astype(float)/1000.
+    obs = obs.loc[(obs['lat']>=latmin) & (obs['lat']<=latmax) & (obs['lon']>=lonmin) & (obs['lon']<=lonmax)]  # Select area
+    obs.set_index(['num_poste','date'], inplace=True)
+
+    return obs.to_xarray()
+
+def read_lpn():
+    lpn = pd.read_csv(os.path.join(datadir, 'LPN_nivometeo_20210801_20220801.csv'), sep=';', parse_dates=['H_NIVO.DAT'], dtype={'H_NIVO.NUM_POSTE':int, 'H_NIVO.ALTI_LPNX':int})
+    lpn.rename(columns={'H_NIVO.ALTI_LPNX':'LPNX', 'H_NIVO.NUM_POSTE':'num_poste', 'H_NIVO.DAT':'date'}, inplace=True)
+
+    return lpn
+
+def read_antilope():
+    #filename = 'ANTILOPEQ_2021073106_2022070106_GrandesRousses.nc'
+    filename = 'ANTILOPEH_2021103000_2022060200_alp.nc'
+    antilope = xr.open_dataset(os.path.join(datadir, filename))
+    if filename.startswith('ANTILOPEH'):
+        # Convert hourly precipitation into 24h precipitation between 6h J-1 and 6h J
+        # Problem : the xarray tools to do that allows only accumulations between
+        # 0h and 23h.
+        # solution : shift time serie by 7h, compute 24h accumulations and
+        # shift back !
+        antilope['time'] = antilope.time-np.timedelta64(7, 'h')
+        antilope = antilope.resample(time='1D').sum(dim='time')  # !!! VERY SLOW !!! WARNING : does not work with pandas>=2.0.0
+        antilope['time'] = antilope.time+np.timedelta64(30, 'h')
+
+    return antilope
+
+def read_corrected_antilope():
+    filename = 'ANTILOPEQ_2021120106_2022050106_alp_corrected.nc'
+    antilope = xr.open_dataset(os.path.join(datadir, filename))
+    if filename.startswith('ANTILOPEH'):
+        # Convert hourly precipitation into 24h precipitation between 6h J-1 and 6h J
+        # Problem : the xarray tools to do that allows only accumulations between
+        # 0h and 23h.
+        # solution : shift time serie by 7h, compute 24h accumulations and
+        # shift back !
+        antilope['time'] = antilope.time-np.timedelta64(7, 'h')
+        antilope = antilope.resample(time='1D').sum(dim='time')  # !!! VERY SLOW !!! WARNING : does not work with pandas>=2.0.0
+        antilope['time'] = antilope.time+np.timedelta64(30, 'h')
+
+    return antilope
+
+if __name__ == "__main__":
+
+    # Read reference observations
+    nivometeo = read_nivometeo_obs()
+    nivometeo = nivometeo.transpose()
+    lats = nivometeo.lat.groupby('num_poste').mean('date').data
+    lons = nivometeo.lon.groupby('num_poste').mean('date').data
+
+    # Read raw ANTILOPE data
+    antilope = read_antilope()
+    # Extraction of evaluation points
+    ant = antilope.sel(lat=xr.DataArray(lats, dims='num_poste'), lon=xr.DataArray(lons, dims='num_poste'), method='nearest').sel(time=nivometeo.date)
+
+    labels = list()
+
+    position = 1  # Positions of violinplots
+    fig, ax = plt.subplots(figsize=(10,10))
+
+    #bias = scores.bias(ant.rr.data, nivometeo.obs.data)  # Raw ANTILOPE bias
+    bias = np.nanmean(ant.rr.data-nivometeo.obs.data, axis=0)  # Raw ANTILOPE bias
+    #mask = np.where(nivometeo.obs.data>0)
+    #rat = np.nanmean(ant.rr.data[mask]/nivometeo.obs.data[mask], axis=0)  # Raw ANTILOPE ratio
+    labels.append(scores.violinplot(ax, position, bias, 'Raw ANTILOPE'))
+
+    for suffix in ['arome', 'sans_arome', 'nivometeo_arome', 'nivometeo_sans_arome']:
+        position = position + 1
+
+        ratio  = xr.open_dataset(os.path.join(workdir, f"Estimated_ratio_alp_0.15_{suffix}_ref.nc"))
+        #error = xr.open_dataset(os.path.join(workdir, "Observation_error_0.15_alp.nc"))
+
+        rat = ratio.sel(lat=xr.DataArray(lats, dims='num_poste'), lon=xr.DataArray(lons, dims='num_poste'), method='nearest').ratio
+        #err = error.sel(lat=xr.DataArray(lats, dims='num_poste'), lon=xr.DataArray(lons, dims='num_poste'), method='nearest').error
+
+        #bias = scores.bias(ant.rr.data/rat.data, nivometeo.obs.data)  # ANTILOPE bias after debiasing
+        bias = np.nanmean(ant.rr.data/rat.data-nivometeo.obs.data, axis=0)  # ANTILOPE bias after debiasing
+        labels.append(scores.violinplot(ax, position, bias, f'Method {suffix}'))
+
+    ax.set_ylabel(f'Bias', fontsize=28)
+    #ax.set_xticklabels([''] + products, fontsize=28)
+    ax.set_xticks(range(position))
+    ax.yaxis.set_tick_params(labelsize=28)
+    ax.legend(*zip(*labels), fontsize=18)
+    fig.savefig(f'{workdir}/bias.pdf', format='pdf',  bbox_inches='tight')
+
---
+++ scripts/scores.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Auteur: Matthieu Vernay
+# Date : 02/02/2022
+
+import os, sys
+import glob
+import time
+from datetime import datetime,timedelta
+import numpy as np
+import xarray as xr
+import pandas as pd
+from scipy.stats import rankdata
+import CRPS.CRPS as pscore
+
+import argparse
+
+import matplotlib as mpl
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def ensemble_attributes():
+    disp = dispersion()
+
+@property
+def mean(ensemble):
+    return ensemble.mean(axis=3).rr.data
+
+def mean_error(simu, obs):
+    return simu.mean(axis=1) - obs
+
+def median_error(simu, obs):
+    return simu.median(axis=1) - obs
+
+def bias(simu, obs, *args, **kw):
+
+    simu = simu[~np.isnan(obs)]
+    obs = obs[~np.isnan(obs)]
+
+    if np.shape(simu) == np.shape(obs):  # "Simulation" déterministe
+        bias = simu - obs
+    else:  # Simulation s'ensmble
+        bias = mean_error(simu, obs)
+
+    return np.nanmean(bias)
+
+def error_frequency(simu, obs, treshold=0.2, *args, **kw):
+
+    simu = simu[~np.isnan(obs)]
+    obs = obs[~np.isnan(obs)]
+
+    if np.shape(simu) == np.shape(obs):  # "Simulation" déterministe
+        bias = simu - obs
+    else:  # Simulation s'ensmble
+        bias = mean_error(simu, obs)
+
+    error_above_treshold = np.where((simu>=obs*(1+treshold)) & (simu<=obs*(1-treshold)))
+    #freq_error = (np.count_nonzero(error_above_treshold) / len(error_above_treshold) * 100
+    freq_error = (len(error_above_treshold) / len(obs)) * 100
+
+    return freq_error
+
+def dispersion(ensemble):
+    """
+    spread over all dates (and pixels ?)
+    """
+    disp = np.sqrt(np.mean([np.nanmean((ensemble.loc[{'member':m}].rr.data-mean(ensemble))**2) for m in ensemble.member.data]))
+    print('Dispersion = ', disp)
+
+    return disp
+
+def rmse(simu, obs, *args, **kw):
+
+    simu = simu[~np.isnan(obs)]
+    obs = obs[~np.isnan(obs)]
+
+    if np.shape(simu) == np.shape(obs):  # "Simulation" déterministe
+        rmse = np.sqrt(np.nanmean(np.square(simu-obs))) if np.nanmean(np.square(simu-obs)) > 0 else np.nan
+    else:  # Simulation d'ensemble
+        rmse = np.sqrt(np.nanmean(np.square(np.median(simu, axis=1) - obs)))
+    return rmse
+
+def brier_skill_score(simu, obs, ref, threshold=10):
+    """  BSS = 1 - BS / BSref  """
+    return 1 - brier(simu, obs, threshold) / brier(ref, obs, threshold)
+
+def brier(simu, obs, Ne=16, threshold=10, *args):
+
+    # TODO : verifier le calcul du score de brier
+    simu = simu[~np.isnan(obs)]
+    obs = obs[~np.isnan(obs)]
+
+    if np.shape(simu) == np.shape(obs):  # "Simulation" déterministe
+        psimu = np.where(simu>=threshold, 1, 0)
+    else:  # Simulation d'ensemble
+        psimu  = np.count_nonzero(simu>=threshold, axis=1) / Ne
+        #psimu  = (np.count_nonzero(simu>=threshold, axis=1)+ 2/3) / (Ne+4/3)  # Tukey's plotting position
+    fobs   = np.where(obs>=threshold, 1, 0)
+    brier = np.nanmean((psimu-fobs)**2)
+    #print('Brier=',brier)
+
+    return brier
+
+def CRPS(simu, obs, *args):
+
+    crps = list()
+    for i in range(len(obs)):
+        if len(np.shape(simu)) == 1:
+            crps.append(pscore([simu[i]], obs[i]).compute()[0])
+        else:
+            crps.append(pscore(simu[i], obs[i]).compute()[0])
+
+    return np.nanmean(np.array(crps))
+
+def ROC(simu, obs, product, ax, Ne=16, threshold=10):
+    """ 
+    Here "probability" is the forecasted probability above which the
+    event is considered well forecasted by the ensemble.
+    We built the contingency table :
+        - a = forecasted and observed
+        - b = forecasted but not observed
+        - c = observed but not forecasted
+        - d = Not forecasted and not observed
+
+    Then the success rate is a/(a+c) and the false alarm rate is b/(b+d)
+    """
+    simu = simu[~np.isnan(obs)]
+    obs  = obs[~np.isnan(obs)]
+
+    #linestyle_map = {1:':', 10:'-', 20:'--'}
+    #color = next(ax._get_lines.prop_cycler)['color']
+
+    # TODO : Use Tukey's plotting probabilities 
+
+    succes_rate = list()
+    false_alarm = list()
+    if np.shape(simu) == np.shape(obs):
+        a = np.count_nonzero(np.where((obs>threshold) & (simu>threshold)))
+        b = np.count_nonzero(np.where((obs<=threshold) & (simu>threshold)))
+        c = np.count_nonzero(np.where((obs>threshold) & (simu<=threshold)))
+        d = np.count_nonzero(np.where((obs<=threshold) & (simu<=threshold)))
+        succes_rate.append(a/(a+c) if a>0 else 0)
+        false_alarm.append(b/(b+d) if b>0 else 0)
+    else:
+        for seuil in range(1, Ne+1):
+            # Pour un dépassement de seuil :
+            a = len(np.where((obs>threshold) & (np.count_nonzero(simu>threshold, axis=1)>=seuil))[0])
+            b = len(np.where((obs<=threshold) & (np.count_nonzero(simu>threshold, axis=1)>=seuil))[0])
+            c = len(np.where((obs>threshold) & (np.count_nonzero(simu>threshold, axis=1)<seuil))[0])
+            d = len(np.where((obs<=threshold) & (np.count_nonzero(simu>threshold, axis=1)<seuil))[0])
+#            # Pour un intervalle :
+#            a = np.count_nonzero(np.where((obs>=1) & (obs<5) & (np.count_nonzero((simu>=1) & (simu<5), axis=1)>=seuil)))
+#            b = np.count_nonzero(np.where(((obs<1) | (obs>=5)) & (np.count_nonzero((simu>=1) & (simu<5), axis=1)>=seuil)))
+#            c = np.count_nonzero(np.where((obs>=1) & (obs<5) & (np.count_nonzero((simu>=1) & (simu<5), axis=1)<seuil)))
+#            d = np.count_nonzero(np.where(((obs<1) | (obs>=5)) & (np.count_nonzero((simu>=1) & (simu<5), axis=1)<seuil)))
+
+            succes_rate.append(a/(a+c) if a+c>0 else np.nan)  # a+c=0 if the event is never observed
+            false_alarm.append(b/(b+d) if b+d>0 else np.nan)  # b+d= 0 if the event is always observed
+
+    #plt.plot(false_alarm, succes_rate, label=product, linestyle=linestyle_map[threshold])
+    if np.shape(simu) == np.shape(obs):
+        #ax.plot(false_alarm, succes_rate, marker = '+', markersize=12, linestyle='', label=product, color='k')
+        ax.plot(false_alarm, succes_rate, marker = '+', markersize=12, linestyle='', label=product)
+    else:
+        ax.plot(false_alarm, succes_rate, label=product)
+
+    return (false_alarm, succes_rate)
+
+def rank_histogram(ensemble, obs, product, ax, *args):
+    """
+    Inspired from : https://github.com/oliverangelil/rankhistogram/blob/master/ranky.py
+    When two or more forecasts have same value (most commonly 0), random selection is made for which bin receives the count.
+    """
+#        maxsim = np.amax(simu, 1)
+    # WARNING : la condition obs>0 réduit PLUS le nombre de cas.
+    # Le choix de la condition est très important car il fait apparaitre ou disparaitre
+    # une énorme majorité des situations où tout est à 0 sauf 1 membre (grand pic à gauche de l'histogramme)
+#        simu = simu[obs>0]
+#        obs = obs[obs>0]
+#        simu = simu[(~np.isnan(obs)) & ((obs>0) | (maxsim>0))]
+#        obs = obs[(~np.isnan(obs)) & ((obs>0) | (maxsim>0))]
+#        position = np.array([])
+#        for idx, obs in enumerate(obs):
+#            position = np.append(position, np.searchsorted(np.sort(simu[idx]), obs))
+#        ax.hist(position, bins=range(np.shape(simu)[1]))
+
+    ensemble = np.transpose(ensemble)  # Shape (Nmember, Ndates)
+    ensemble = ensemble[:,~np.isnan(obs)]
+    obs = obs[~np.isnan(obs)]
+    combined = np.vstack((obs[np.newaxis], ensemble))
+
+    # Computing ranks
+    ranks = np.apply_along_axis(lambda x: rankdata(x, method='min'), 0, combined)
+
+    # Computing ties')
+    ties = np.sum(ranks[0]==ranks[1:], axis=0)
+    ranks = ranks[0]
+    tie = np.unique(ties)
+
+    for i in range(1,len(tie)):
+        index = ranks[ties==tie[i]]
+        # print('randomizing tied ranks for ' + str(len(index)) + ' instances where there is ' + str(tie[i]) + ' tie/s. ' + str(len(tie)-i-1) + ' more to go')
+        ranks[ties==tie[i]] = [np.random.randint(index[j], index[j]+tie[i]+1, tie[i])[0] for j in range(len(index))]
+
+    #return np.histogram(ranks, bins=np.linspace(0.5, combined.shape[0]+0.5, combined.shape[0]+1))
+    #ax.hist(ranks, bins=range(np.shape(ensemble)[0]))
+    ax.hist(ranks, bins=np.linspace(0.5, combined.shape[0]+0.5, combined.shape[0]+1))
+
+def reliability_diagram(simu, obs, product, ax, Ne=16):
+    ndays = len(obs)
+    simu = simu[~np.isnan(obs)]
+    obs = obs[~np.isnan(obs)]
+    proba, catsize, freq_occ, global_freq_occ = probability_classes(simu, obs, Ne=Ne)
+    # TODO : taille du marker proportionelle au nombre de prevision dans une categorie
+    ax.plot(proba, freq_occ, marker=None, linestyle='-', label=f'{product}')
+    ax.scatter(proba, freq_occ, catsize)
+
+def probability_classes(simu, obs, treshold=10, Ne=16, nb_cat=17):
+
+    # TODO : la décomposition du score de Brier devrait donner le même résultat
+    # que le calcul direct (BS=BSfiab-BSres+BSunc), mais ce n'est pas le cas...
+
+    catsize  = list()
+    freq_occ = list()
+    proba    = list()
+    for Nm in range(nb_cat):
+        Ni = np.count_nonzero(np.count_nonzero(simu>=threshold, axis=1)==Nm)
+        if Ni > 0:
+            proba.append(Nm/Ne)
+            catsize.append(Ni)
+            # TODO : problème avec les dimensions de "simu" lorsque simu est un ensemble...
+            freq_occ.append(np.count_nonzero(obs[np.count_nonzero(simu>=threshold, axis=1)==Nm]>=threshold)/Ni)
+    ndays = len(obs)
+    global_freq_obs = np.count_nonzero(obs[obs>=threshold]) / ndays
+
+    return np.array(proba), np.array(catsize), np.array(freq_occ), global_freq_obs
+
+def reliability(simu, obs, *args):
+    simu = simu[~np.isnan(obs)]
+    obs = obs[~np.isnan(obs)]
+    ndays = len(obs)
+    proba, catsize, freq_occ, global_freq_occ = probability_classes(simu, obs)
+    reliability = 1/ndays*np.sum(catsize*(proba-freq_occ)**2)
+    #reliability = 1/ndays*np.sum([Ni*(proba-focc)**2 for (proba, Ni, focc) in zip(proba, catsize, freq_occ)])  # Equivalent
+    print('reliability=',reliability)
+    return reliability
+
+def resolution(simu, obs, *args):
+    simu = simu[~np.isnan(obs)]
+    obs = obs[~np.isnan(obs)]
+    ndays = len(obs)
+    proba, catsize, freq_occ, global_freq_obs = probability_classes(simu, obs)
+    resolution = 1/ndays*np.sum(catsize*(freq_occ-global_freq_obs)**2)
+    #resolution = 1/ndays*np.sum([Ni*(focc-global_freq_obs)**2 for (Ni, focc) in zip(catsize, freq_occ)])  # Equivalent
+    print('Resolution=',resolution)
+    return resolution
+
+def uncertainty(simu, obs, *args):
+    simu = simu[~np.isnan(obs)]
+    obs = obs[~np.isnan(obs)]
+    proba, catsize, freq_occ, global_freq_obs = probability_classes(simu, obs)
+    uncertainty =  global_freq_obs*(1-global_freq_obs)**2
+    print('Uncertainty=',uncertainty)
+    return uncertainty
+
+def violinplot(ax, position, data, label, color=None):
+    """ Customize violinplot by adding a label"""
+    import matplotlib.patches as mpatches
+
+    ax.plot([position]*len(data), data, linestyle='', marker='_', markersize='20', color='k')
+    violin = ax.violinplot(data, positions=[position], showmeans=True)
+    ax.axhline(color='k')
+    if color is None:
+        color = violin["bodies"][0].get_facecolor().flatten()
+    else:
+        violin["bodies"][0].set_facecolor(color)
+        violin["bodies"][0].set_edgecolor(color)
+
+
+    return (mpatches.Patch(color=color), label)
+
+
