4927ba0d18996b01a1eff81079acc39bd3b6935a
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2023-08-23 21:27:19 +0200

    Draw random values from a gamma law instead of a normal one

--- radar/Preprocessing_ANTILOPE.py
+++ radar/Preprocessing_ANTILOPE.py
@@ -160,6 +160,7 @@ def codistances(coords, ld=0.07):
 
 def random_draw(obs, sd):
     gauss = np.random.normal(loc=0.0, scale=1.0, size=1)[0]  # Draw random element from normal distribution
+    gamma = np.random.gamma(1, scale=1)  # Draw random element from normal distribution (>0 only ==> shift necessary to convert into perturbations)
     exp = np.random.default_rng().exponential(scale=1)  # TODO : set scale parameter using the density of pixels at 0mm in the vicinity ?
 
     # Ensure that RR are >=0
@@ -169,10 +170,11 @@ def random_draw(obs, sd):
     #sd = np.sqrt(sd)  # sigma --> sigma² dans la formulation de la loi normale
     # TODO : comprendre pourquoi la conversion R^1/2 --> R disperse autant l'ensemble
     #ana = np.square(obs)+gauss*sd  # Gaussian perturbation around >0 obs
-    ana = obs+gauss*sd  # Gaussian perturbation around >0 obs
+    #ana = obs+gauss*sd  # Gaussian perturbation around >0 obs
+    ana = obs+(gamma-1)*sd  # Shift gamma distribution so that the mode ((k-1)*theta=k-1) is on 0
     #ana[ana<0] = exp*sd[ana<0]  # Avoid "mass accumulation" in 0. !! WARNING : the analysis distribution is not Normal anymore !!
-    # TODO : different treatment of values <1 and >1 since conversion RR^1/2 --> RR completly changes the behavior
     ana[ana<0] = 0  # WARNING : "mass accumulation" in 0 (analysis distribution not normal anymore)
+    #ana[np.where(sd<=1)] = obs[np.where(sd<=1)]+gauss*sd[np.where(sd<=1)]  # Gaussian perturbations around pixels with for small errors
     ana[obs==0] = obs[obs==0]+exp*sd[obs==0]  # Exponential perturbation arround 0. TODO : arround 0, use the density
     ana = np.round(ana, 1)
     # of pixels at 0mm in the vicinity instead of sd ?
