0162c27400621aaff04a4158d45d1a766e8a1d2a
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-01-11 14:48:19 +0100

    Generate a 16 member ensemble of gridded FORCING files at 250 resolution
    - Over the Grandes Rousses domain for the season 2021/2022
    - Precipitation come from RS25 ensemble analysis
    - LNP computed with the VV method and downscalled by SR
    - Other parameters come from SAFRAN reanalysis and projected on the 250m
      grid

--- scripts/make_forcing.py
+++ scripts/make_forcing.py
@@ -14,47 +14,90 @@ from bronx.stdtypes.date import Date, Period
 
 toolbox.active_now = True
 
+# Define VORTEX cache if not already in the environment (local only)
+#t = vortex.ticket()
+#t.env.setvar('WORKDIR', '/home/vernaym/workdir')
 
-datadir =  '/home/vernaym/workdir/EDELWEISS'
+datebegin = '2021080206'
+dateend   = '2022080106'
+
+#datadir =  '/home/vernaym/workdir/EDELWEISS'  # local
+datadir =  '/home/mrns/vernaym/workdir/EDELWEISS'  # soprano
 
 # 1. Radiation variables come from SAFRAN reanalysis in the initial stage (similar for all ensemble members)
 # TODO : à récupérer avec Vortex
-#radiation = xr.open_dataset(os.path.join(datadir, 'SAFRAN_to_grid', 'meteo', 'SAFRAN_RADIATION_2021122706_2021123006_gr250ls.nc'))
-radiation = xr.open_dataset(os.path.join(datadir, 'SAFRAN_to_grid', 'meteo', 'FORCING_2021122706_2021123006_gr250ls.nc'))
+#forcing = xr.open_dataset(os.path.join(datadir, 'SAFRAN_to_grid', 'meteo', 'SAFRAN_RADIATION_2021122706_2021123006_gr250ls.nc'))
+#forcing = xr.open_dataset(os.path.join(datadir, 'SAFRAN_to_grid', 'meteo', 'FORCING_2021122706_2021123006_gr250ls.nc'))
+forcing = xr.open_dataset(os.path.join(datadir, 'SAFRAN_to_grid', 'meteo', 'FORCING_2021080106_2022080106_gr250ls.nc'))
 
 # 2. Precipitation come from MV's ensemble analysis, downscaled by SR with the method from VV
-# TODO : à récupérer avec Vortex
-# TODO : Loop over ensemble members
-#filename = os.path.join(datadir, 'hourly_precipitation_analysis', 'Precipittaion_2021122706_2021123006.nc')
-#tbin = toolbox.output(
-#        role        = 'Precipitation analysis',
-#        kind        = 'Precipitation',
-#        vapp        = 'edelweiss',
-#        vconf       = '[geometry:area]',
-#        source_app  = 'antilope',
-#        source_conf ='RandomSampling',
-#        cutoff      = 'assimilation',
-#        filename    = filename,
-#        experiment  = 'XP25@radanovicss',
-#        geometry    = 'GrandesRousses250m',
-#        nativefmt   = 'netcdf',
-#        model       = 'edelweiss',
-#        date        = enddate.ymd6h,
-#        datebegin   = startdate.ymd6h,
-#        dateend     = enddate.ymd6h,
-#        namespace   = 'vortex.multi.fr',
-#        member      = footprints.util.rangex(1, 16, 1),
-#        block       = 'analysis',
-#        intent      = 'inout',
-#    )
-precipitation = xr.open_dataset(os.path.join(datadir, 'hourly_precipitation_analysis', 'precipitation.antilope-randomsampling_2021122706_2021123006.nc'))
-precipitation=precipitation.rename({'xx':'x', 'yy':'y'})
+dirname = os.path.join(datadir, 'meteo')
+filename = 'Precipitation_2021080206_2022080106.nc'
+tbin = toolbox.input(
+        role        = 'Precipitation analysis',
+        kind        = 'Precipitation',
+        vapp        = 'edelweiss',
+        vconf       = '[geometry:area]',
+        source_app  = 'antilope',
+        source_conf = 'RandomSampling',
+        cutoff      = 'assimilation',
+        filename    = f'{dirname}/mb[member]/{filename}',
+        experiment  = 'XP25@radanovicss',
+        geometry    = 'GrandesRousses250m',
+        nativefmt   = 'netcdf',
+        namebuild   = 'flat@cen',
+        model       = 'edelweiss',
+        #date        = enddate.ymd6h,
+        #datebegin   = startdate.ymd6h,
+        #dateend     = enddate.ymd6h,
+        date        = '2022080106',
+        datebegin   = '2021080206',
+        dateend     = '2022080106',
+        namespace   = 'vortex.multi.fr',
+        member      = footprints.util.rangex(1, 16, 1),
+        block       = 'analysis',
+        intent      = 'inout',
+    )
 
-# Replace Rainf/Snowf variables by the ones from the ensemble analysis
-radiation['Rainf'] = precipitation['Rainf_ds']
-radiation['Snowf'] = precipitation['Snowf_ds']
+# Concatenation of all FORCING variables into the final FORCING files
+outname = 'FORCING_2021080206_2022080106_gr250ls.nc'
+for member in range(1,17):
+    if not os.path.exists(os.path.join(datadir, 'meteo', f'mb{member:03d}', outname)):
+        #precipitation = xr.open_dataset(os.path.join(datadir, 'hourly_precipitation_analysis', 'precipitation.antilope-randomsampling_2021122706_2021123006.nc'))
+        precipitation = xr.open_dataset(os.path.join(dirname, f'mb{member:03d}', filename))
+        precipitation=precipitation.rename({'xx':'x', 'yy':'y'})
 
-radiation.to_netcdf(os.path.join(datadir, 'meteo', 'FORCING_2021122706_2021123006_gr250ls.nc'))
+        # Replace Rainf/Snowf variables by the ones from the ensemble analysis
+        forcing['Rainf'] = precipitation['Rainf_ds']
+        forcing['Snowf'] = precipitation['Snowf_ds']
+
+        forcing.to_netcdf(os.path.join(datadir, 'meteo', f'mb{member:03d}', outname))
+
+tbin = toolbox.input(
+        role        = 'Forcing file',
+        kind        = 'MeteorologicalForcing',
+        vapp        = 'edelweiss',
+        vconf       = '[geometry:area]',
+        source_app  = 'antilope',
+        source_conf = 'RandomSampling',
+        cutoff      = 'assimilation',
+        filename    = f'{datadir}/meteo/mb[member]/{outname}',
+        experiment  = 'XP25@vernaym',
+        geometry    = 'GrandesRousses250m',
+        nativefmt   = 'netcdf',
+        namebuild   = 'flat@cen',
+        model       = 'edelweiss',
+        #date        = enddate.ymd6h,
+        #datebegin   = startdate.ymd6h,
+        #dateend     = enddate.ymd6h,
+        date        = '2022080106',
+        datebegin   = '2021080206',
+        dateend     = '2022080106',
+        namespace   = 'vortex.multi.fr',
+        member      = footprints.util.rangex(1, 16, 1),
+        block       = 'analysis',
+        intent      = 'inout',
+    )
 
 import pdb
 pdb.set_trace()
