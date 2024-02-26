f50653cb35e300a0b42eea3ad93e1c45cb4e0c45
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-02-26 13:26:53 +0100

    Combine all ensemble members in the 'simulation' product

--- scripts/evaluation_vs_sentinel2.py
+++ scripts/evaluation_vs_sentinel2.py
@@ -5,9 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-simu = xr.open_dataset('mb001/DIAG.nc', decode_times=False)
-simu = simu.rename({'xx':'x', 'yy':'y'})
 mntdir = '/home/vernaym/These/DATA'
+prodir = '/home/vernaym/workdir/EDELWEISS/pro'
 
 def compare(obs, var='LCSMOD'):
     plt.imshow(np.flipud(simu[var].data))
@@ -35,14 +34,23 @@ def plot(obs, var='LCSMOD'):
     mnt = read_mnt()
 
     filtered_obs = per_alt(obs.Band1, altitude_bands, mnt)
-    # TODO : résoudre le problème de décallage des coordonnées
-    simu['x']=mnt['x']
-    simu['y']=mnt['y']
-    filtered_simu = per_alt(simu[var], altitude_bands, mnt)
-
-    simu_df = filtered_simu.to_dataframe(name=var).dropna().reset_index()
     obs_df = filtered_obs.to_dataframe(name=var).dropna().reset_index()
-    dataplot = pd.concat([obs_df, simu_df], keys=['obs', 'simu']).drop(columns=['x','y'])  # unecessarilly large DataFrame (duplicate index) ?
+
+    simu_df= None
+    for member in range(1, 17):
+        simu = xr.open_dataset(f'mb{member:03d}/DIAG.nc', decode_times=False)
+        simu = simu.rename({'xx':'x', 'yy':'y'})
+        # TODO : résoudre le problème de décallage des coordonnées
+        simu['x']=mnt['x']
+        simu['y']=mnt['y']
+        filtered_simu = per_alt(simu[var], altitude_bands, mnt)
+        df = filtered_simu.to_dataframe(name=var).dropna().reset_index()
+        if simu_df is not None:
+            simu_df = pd.concat([simu_df, df])
+        else:
+            simu_df = df
+
+    dataplot = pd.concat([simu_df, obs_df], keys=['simu', 'obs']).drop(columns=['x','y'])  # unecessarilly large DataFrame (duplicate index) ?
     #dataplot = pd.concat([obs_df, simu_df], axis=1)  # TODO : drop duplicated x/y/middle_slices_ZS columns
     #dataplot = pd.concat([obs_df, simu_df['simu']], axis=1)  # WARNING : this does not ensure that x/y/middle_slices_ZS columns match !
     dataplot.columns = dataplot.columns.str.replace('middle_slices_ZS', 'Elevation Bands (m)')
@@ -52,7 +60,7 @@ def plot(obs, var='LCSMOD'):
     sns.set(rc={"figure.figsize":(12, 15)})
     sns.set_theme(style="whitegrid",font_scale=1.7)
     g=sns.violinplot(dataplot.reset_index().rename(columns={'level_0': 'forcing'}) ,y='Elevation Bands (m)',x=var,inner='box',hue='forcing',scale='width',bw='scott',\
-               cut=0,orient='h',palette=("#6ACC64","silver",),facet_kws={'legend_out': True})
+               cut=0,orient='h',palette=("#6ACC64", "silver"),facet_kws={'legend_out': True})
     plt.ylim(reversed(plt.ylim()))
 
     plt.show()
@@ -89,6 +97,8 @@ if __name__ == '__main__':
 
     var = 'LCSMOD'
     #var = 'LCSCD'
+
+    os.chdir(prodir)
 
     if var == 'LCSMOD':
         obs = xr.open_dataset('/home/vernaym/These/DATA/Sentinel2/20210901_L3B-SNOW_SMD_R2.nc')
