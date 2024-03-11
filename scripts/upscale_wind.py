54bf41780fd5708c85c624f832f3bc83a245b49e
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-03-11 17:12:35 +0100

    DBUG and clean up code to respect PEP8 norm

--- scripts/upscale_wind.py
+++ scripts/upscale_wind.py
@@ -8,19 +8,12 @@ import glob
 
 import xarray as xr
 import pandas as pd
-import rioxarray
-import rasterio
 import numpy as np
-import pyresample
-from pyresample.bucket import BucketResampler
 
-import vortex
-from vortex import toolbox
-import cen
+from snowtools.scripts.extract.vortex import vortexIO
 
-toolbox.active_now = True
 
-DEFAULT_NETCDF_FORMAT = 'NETCDF3_CLASSIC'
+DEFAULT_NETCDF_FORMAT = 'NETCDF4_CLASSIC'
 
 # This script upscales 30m wind fields produced by Louis Le Toumelin's method to an EDELWEISS's 250m grid
 # Input data are monthly Netcdf files at 30m resolution with wind 'speed' and 'direction' variables.
@@ -30,14 +23,20 @@ DEFAULT_NETCDF_FORMAT = 'NETCDF3_CLASSIC'
 # -----------------------------
 # 1. Concatenate all data into 1 single xarray dataset to avoid duplicates or missing data
 # 2. Apply 'wind2comp' method to get U and V wind components
-# 3. Upscale each component separately with rioxarray "reproject_match" function and "average" option (https://corteva.github.io/rioxarray/html/examples/reproject_match.html)
+# 3. Upscale each component separately with rioxarray "reproject_match" function and "average" option
+#    (https://corteva.github.io/rioxarray/html/examples/reproject_match.html)
 # 4. Convert U and V wind components into 'Wind' and 'Wind_dir' with 'comp2speed' et 'comp2dir' methods
 # 5. Update netcdf attributes
 # 6. Archive output netcdf file on hendrix with Vortex
 
-datadir = '/home/merzisenh/NO_SAVE/arome_2021_2022_downscaled_devine/downscaled'  # Extraction HM de decembre 2023 from 2021070106 to 2022073123
-#workdir = '/home/vernaym/workdir/EDELWEISS/wind'
+# Extraction HM de decembre 2023 from 2021070106 to 2022073123
+datadir = '/home/merzisenh/NO_SAVE/arome_2021_2022_downscaled_devine/downscaled'
+# workdir = '/home/vernaym/workdir/EDELWEISS/wind'
 workdir = '/cnrm/cen/users/NO_SAVE/vernaym/workdir/EDELWEISS/wind'
+
+# * Source des fonctions LLT :
+# https://github.com/louisletoumelin/bias_correction/blob/12e806af084d086d30e429b21deb8ab7f243a381/bias_correction/train/wind_utils.py
+
 
 # Fonction LLT*
 def wind2comp(uv, dir, unit_direction="radian"):
@@ -47,12 +46,14 @@ def wind2comp(uv, dir, unit_direction="radian"):
     v = -np.cos(dir) * uv
     return u, v
 
+
 # Fonction LLT*
 def comp2dir(u, v, unit_output="degree"):
     if unit_output == "degree":
         return np.mod(180 + np.rad2deg(np.arctan2(u, v)), 360)
     else:
         raise NotImplementedError
+
 
 # Fonction LLT*
 def comp2speed(u, v, w=None):
@@ -61,7 +62,6 @@ def comp2speed(u, v, w=None):
     else:
         return np.sqrt(u ** 2 + v ** 2 + w ** 2)
 
-# * Source des fonctions LLT : https://github.com/louisletoumelin/bias_correction/blob/12e806af084d086d30e429b21deb8ab7f243a381/bias_correction/train/wind_utils.py
 
 def upscale(filename):
     """
@@ -69,64 +69,28 @@ def upscale(filename):
     """
 
     suffix = '_'.join(filename.split('.')[0].split('_')[-2:])
-    if not (os.path.exists(os.path.join(workdir, f'Wind_gr250m_{suffix}.nc')) or os.path.exists(os.path.join(workdir, f'Wind_dir_gr250m_{suffix}.nc'))):
+    if not (os.path.exists(os.path.join(workdir, f'Wind_gr250m_{suffix}.nc')) or
+            os.path.exists(os.path.join(workdir, f'Wind_dir_gr250m_{suffix}.nc'))):
         # 1. Open monthly 30m wind Netcdf files produced by HM into 1 single xarray dataset and add projection
         wind30m = xr.open_dataset(filename)
-        #wind30m = xr.open_mfdataset(os.path.join(datadir, '*'))
-        #wind30m = xr.open_mfdataset(os.path.join(datadir, '*'), chunks={'x': 500, 'y': 500})
         wind30m = wind30m.rio.write_crs(2154)  # 3857 ?
-        #wind30m = wind30m.chunk({'time':100, 'x': 10, 'y': 10})
-        wind30m = wind30m.chunk({'time':100})
-        # TODO : optimisation of chunk sizes necessary
-        #wind30m.to_zarr(os.path.join(workdir, 'wind30m.zarr'), mode='w')
+        wind30m = wind30m.chunk({'time': 100})
 
         # 2. Compute U and V wind components
         u30m, v30m = wind2comp(wind30m.speed, wind30m.direction, unit_direction="degree")
 
-        #u30m.to_zarr(os.path.join(workdir, 'u30m.zarr'))
-        #v30m.to_zarr(os.path.join(workdir, 'v30m.zarr'))
-
-        #print('Read u30m')
-        #u30m = xr.open_zarr(os.path.join(workdir, 'u30m.zarr'), chunks={'time':1000, 'x': 500, 'y': 500})
-        #u30m = xr.open_zarr(os.path.join(workdir, 'u30m.zarr'))
-        #print('Read v30m')
-        #v30m = xr.open_zarr(os.path.join(workdir, 'v30m.zarr'), chunks={'time':1000, 'x': 500, 'y': 500})
-        #v30m = xr.open_zarr(os.path.join(workdir, 'v30m.zarr'))
-
         # 3. Project 30m U and V wind components on the 250m grid
 
         # Open 250m grid reference file
-        #gr250m = xr.open_dataarray('/home/vernaym/workdir/EDELWEISS/SAFRAN_to_grid/meteo/FORCING_2021080106_2022080106_gr250m.nc')  # FORCING projected from SAFRAN reanalysis
-        gr250m = xr.open_dataset('/home/vernaym/These/DATA/MNTLouisGRoussecorrected.nc')  # Ange's MNT with elevation adapted to SARAN geometry
+        # Use Ange's MNT with elevation adapted to SARAN geometry
+        gr250m = xr.open_dataset('/home/vernaym/These/DATA/MNTLouisGRoussecorrected.nc')
         gr250m = gr250m.rio.write_crs(2154)
 
-        # With pyresample (NOT WORKING)
-#    source_area = pyresample.geometry.AreaDefinition(  # https://pyresample.readthedocs.io/en/latest/api/pyresample.html#module-pyresample.geometry
-#            'gr30m',  # area_id
-#            'Grandes Rousses domain at 30m resolution',  # description
-#            'WGS84',  #proj_id
-#            'Europe_Lambert_Conformal_Conic',  # projection
-#            len(u30m.x.data),  # width
-#            len(u30m.y.data),  # height
-#            (u30m.x.data[0], u30m.y.data[0], u30m.x.data[-1], u30m.y.data[-1]),  #area_extent
-#        )
-#
-#    target_area = pyresample.geometry.AreaDefinition(  # https://pyresample.readthedocs.io/en/latest/api/pyresample.html#module-pyresample.geometry
-#            'gr250m',  # area_id
-#            'Grandes Rousses domain at 250m resolution',  # description
-#            'WGS84',  #proj_id
-#            'Europe_Lambert_Conformal_Conic',  # projection
-#            len(gr250m.x.data),  # width
-#            len(gr250m.y.data),  # height
-#            (gr250m.x.data[0], gr250m.y.data[0], gr250m.x.data[-1], gr250m.y.data[-1]),  #area_extent
-#        )
-
         # Project 30m fields on 250m grid
-        #lons, lats = source_area.get_lonlats()
-        #resampler = BucketResampler(target_area, lons, lats)  # https://pyresample.readthedocs.io/en/latest/api/pyresample.html
+        # lons, lats = source_area.get_lonlats()
         print(f'Projection U {suffix}')
-        u250m = u30m.rio.reproject_match(gr250m, resamplin="average")  # rioxarray does not support dask : https://github.com/corteva/rioxarray/issues/119 --> memory limit
-        #u250m = resampler.get_average(u30m)
+        # rioxarray does not support dask : https://github.com/corteva/rioxarray/issues/119 --> memory limit
+        u250m = u30m.rio.reproject_match(gr250m, resamplin="average")
         print(f'Projection V {suffix}')
         v250m = v30m.rio.reproject_match(gr250m, resamplin="average")
 
@@ -137,6 +101,7 @@ def upscale(filename):
         wdir250m = comp2dir(u250m, v250m)
         wdir250m.to_netcdf(os.path.join(workdir, f'Wind_dir_gr250m_{suffix}.nc'), format=DEFAULT_NETCDF_FORMAT)
         wdir250m.close()
+
 
 if __name__ == '__main__':
 
@@ -152,35 +117,16 @@ if __name__ == '__main__':
 
         # 2. Read the (multiple) files created and concatenate data into 1 single netcf file
         wind250m = xr.open_mfdataset(glob.glob(os.path.join(workdir, 'Wind_gr250m*.nc')))
-        wind250m = wind250m.rename({'__xarray_dataarray_variable__':'Wind'})
+        wind250m = wind250m.rename({'__xarray_dataarray_variable__': 'Wind'})
         wdir250m = xr.open_mfdataset(glob.glob(os.path.join(workdir, 'Wind_dir_gr250m*.nc')))
-        wdir250m = wdir250m.rename({'__xarray_dataarray_variable__':'Wind_dir'})
+        wdir250m = wdir250m.rename({'__xarray_dataarray_variable__': 'Wind_dir'})
         wind250m = wind250m.merge(wdir250m)
         start = pd.to_datetime(datebegin, format='%Y%m%d%H').to_numpy()
         end = pd.to_datetime(dateend, format='%Y%m%d%H').to_numpy()
         dates = xr.date_range(start=start, end=end, freq='H')
-        wind250m = wind250m.sel({'time':np.intersect1d(dates, wind250m.time)})
+        wind250m = wind250m.sel({'time': np.intersect1d(dates, wind250m.time)})
         wind250m.to_netcdf(outname, format=DEFAULT_NETCDF_FORMAT)
 
-    tbout = toolbox.output(
-        role        = 'Wind',
-        kind        = 'Wind',
-        vapp        = 'edelweiss',
-        vconf       = '[geometry:tag]',
-        source_app  = 'arome',
-        source_conf = '4dvarfr',
-        cutoff      = 'assimilation',
-        filename    = outname,
-        experiment  = 'WHM01@vernaym',  # First experiment produced with Hugo.M wind
-        geometry    = 'GrandesRousses250m',
-        nativefmt   = 'netcdf',
-        namebuild   = 'flat@cen',
-        model       = 'devine',
-        date        = dateend,
-        datebegin   = datebegin,
-        dateend     = dateend,
-        namespace   = 'vortex.multi.fr',
-        block       = 'analysis',
-        intent      = 'inout',
-    )
-
+    wind_xpid = 'WHM01@vernaym',  # First experiment produced with Hugo.M wind
+    geometry  = 'GrandesRousses250m'
+    vortexIO.put_wind(datebegin, dateend, wind_xpid, geometry)
