5708647ffb1c766c4309c611883d604276e5d17f
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-05-22 10:04:10 +0200

    Add script to project array with lat/lon crs to Lambert93

---
+++ scripts/proj_array.py
@@ -0,0 +1,11 @@
+import sys
+import xarray as xr
+
+filename = sys.argv[1]
+
+ds = xr.open_dataset(filename)
+ds.rio.write_crs("EPSG:4326", inplace=True)
+ds  = ds.rename({'lon': 'x', 'lat': 'y'})
+out = ds.rio.reproject("EPSG:3857")
+outname = f"{filename.split('.')[0]}_L93.nc"
+out.to_netcdf(outname)
