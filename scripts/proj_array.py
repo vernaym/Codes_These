40d6df82917d414c7abacbe3369a13306abefd05
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-05-27 20:33:52 +0200

    Use proper projection

--- scripts/proj_array.py
+++ scripts/proj_array.py
@@ -6,6 +6,6 @@ filename = sys.argv[1]
 ds = xr.open_dataset(filename)
 ds.rio.write_crs("EPSG:4326", inplace=True)
 ds  = ds.rename({'lon': 'x', 'lat': 'y'})
-out = ds.rio.reproject("EPSG:3857")
+out = ds.rio.reproject("EPSG:2154")
 outname = f"{filename.split('.')[0]}_L93.nc"
 out.to_netcdf(outname)
