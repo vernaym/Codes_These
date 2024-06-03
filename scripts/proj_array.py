3b963b5b665aa8c7eac9ed964c49012e352bf899
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-06-03 12:27:37 +0200

    Update projection script

--- scripts/proj_array.py
+++ scripts/proj_array.py
@@ -6,6 +6,7 @@ filename = sys.argv[1]
 ds = xr.open_dataset(filename)
 ds.rio.write_crs("EPSG:4326", inplace=True)
 ds  = ds.rename({'lon': 'x', 'lat': 'y'})
+ds = ds.transpose('time', 'y', 'x')
 out = ds.rio.reproject("EPSG:2154")
 outname = f"{filename.split('.')[0]}_L93.nc"
 out.to_netcdf(outname)
