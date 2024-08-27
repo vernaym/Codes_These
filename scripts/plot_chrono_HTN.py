dacb11deebe4e01ab15da26564da2adf19947660
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-08-27 10:53:33 +0200

    Add LPN information on temporal plot to study supposed LPN issue

--- scripts/plot_chrono_HTN.py
+++ scripts/plot_chrono_HTN.py
@@ -50,20 +50,49 @@ year = dateend[:4]
 dates_pleiades = pleiades_map[year]['dates']
 geometry = pleiades_map[year]['geometry']
 
-io.get_pro(datebegin=datebegin, dateend=dateend, xpid='SAFRAN_perturb', vconf=point, geometry='SinglePoint',
-        namespace='vortex.cache.fr', filename='PRO_SAFRAN.nc', vapp='edelweiss')
-io.get_pro(datebegin=datebegin, dateend=dateend, xpid='KRIGING_perturb', vconf=point, geometry='SinglePoint',
-        namespace='vortex.cache.fr', filename='PRO_KRIGING.nc', vapp='edelweiss')
-io.get_pro(datebegin=datebegin, dateend=dateend, xpid='ANTILOPE_pappus', vconf=point, geometry='SinglePoint',
-        namespace='vortex.cache.fr', filename='PRO_ANTILOPE.nc', vapp='edelweiss')
 if datebegin == '2021080106':
     datebegin_RS27 = '2021080207'
+    ymax = 1.3  # Huez 2021/2022
 else:
     datebegin_RS27 = datebegin
+    ymax = 3  # Rochilles Nivose 2017/2018
+
+plot_lpn = False
+
 io.get_pro(datebegin=datebegin_RS27, dateend=dateend, xpid='RS27_sorted_pappus', vconf=point, geometry='SinglePoint',
         namespace='vortex.cache.fr', filename='PRO_RS27.nc',vapp='edelweiss')
-io.get_pro(datebegin=datebegin_RS27, dateend=dateend, xpid='RS27_LPNp200', vconf=point, geometry='SinglePoint',
-        namespace='vortex.cache.fr', filename='PRO_RS27_bis.nc',vapp='edelweiss')
+rs27 = xr.open_dataset('PRO_RS27.nc')
+rs27 = rs27.DSN_T_ISBA
+if plot_lpn:
+    io.get_pro(datebegin=datebegin_RS27, dateend=dateend, xpid='RS27_LPNp200', vconf=point, geometry='SinglePoint',
+            namespace='vortex.cache.fr', filename='PRO_RS27_bis.nc',vapp='edelweiss')
+    rs27_bis = xr.open_dataset('PRO_RS27_bis.nc')
+    rs27_bis = rs27_bis.DSN_T_ISBA
+    io.get_meteo(
+        kind        = 'ISO_WETBT',
+        datebegin   = datebegin,
+        dateend     = dateend,
+        geometry    = "EURW1S40",
+        xpid        = "ExtractionBDAP@vernaym",
+        filename    = 'ISO_TPW.nc',
+        vapp        = 'edelweiss',
+        source_app  = 'arome',
+        source_conf = '3dvarfr',
+    )
+
+else:
+    io.get_pro(datebegin=datebegin, dateend=dateend, xpid='SAFRAN_perturb', vconf=point, geometry='SinglePoint',
+            namespace='vortex.cache.fr', filename='PRO_SAFRAN.nc', vapp='edelweiss')
+    io.get_pro(datebegin=datebegin, dateend=dateend, xpid='KRIGING_perturb', vconf=point, geometry='SinglePoint',
+            namespace='vortex.cache.fr', filename='PRO_KRIGING.nc', vapp='edelweiss')
+    io.get_pro(datebegin=datebegin, dateend=dateend, xpid='ANTILOPE_pappus', vconf=point, geometry='SinglePoint',
+            namespace='vortex.cache.fr', filename='PRO_ANTILOPE.nc', vapp='edelweiss')
+    safran = xr.open_dataset('PRO_SAFRAN.nc')
+    safran = safran.DSN_T_ISBA
+    kriging = xr.open_dataset('PRO_KRIGING.nc')
+    kriging = kriging.DSN_T_ISBA
+    antilope = xr.open_dataset('PRO_ANTILOPE.nc')
+    antilope = antilope.DSN_T_ISBA
 
 xpid_map = {
     '2017080106': 'CesarDB',
@@ -73,44 +102,17 @@ xpid_map = {
 io.get_snow_obs_date(xpid=xpid_map[datebegin], geometry=geometry, date=dates_pleiades,
         vapp='Pleiades', filename='Pleiades_[date:ymdh].nc')
 
-io.get_meteo(
-    kind        = 'ISO_WETBT',
-    datebegin   = datebegin,
-    dateend     = dateend,
-    geometry    = "EURW1S40",
-    xpid        = "ExtractionBDAP@vernaym",
-    filename    = 'ISO_TPW.nc',
-    vapp        = 'edelweiss',
-    source_app  = 'arome',
-    source_conf = '3dvarfr',
-)
-
-
-safran = xr.open_dataset('PRO_SAFRAN.nc')
-safran = safran.DSN_T_ISBA
-kriging = xr.open_dataset('PRO_KRIGING.nc')
-kriging = kriging.DSN_T_ISBA
-antilope = xr.open_dataset('PRO_ANTILOPE.nc')
-antilope = antilope.DSN_T_ISBA
-rs27 = xr.open_dataset('PRO_RS27.nc')
-rs27 = rs27.DSN_T_ISBA
-rs27_bis = xr.open_dataset('PRO_RS27_bis.nc')
-rs27_bis = rs27_bis.DSN_T_ISBA
-time = safran.time
-
-ymax = 1.3  # Huez 2021
-
 fig, ax = plt.subplots(figsize=(14, 4))
-ax.set_ylim([-ymax, ymax])
-#plt.plot(time, safran, label='SAFRAN', color="#1f78b4")
-#plt.plot(time, kriging, label='KRIGING', color="#e31a1c")
-#plt.plot(time, antilope, label='ANTILOPE', color="#b2df8a")
-#plt.plot(rs27.time, rs27, label='AS-ANTILOPE', color="#33a02c")
-#plt.plot(time, safran, label='SAFRAN', color="grey")
-#plt.plot(time, kriging, label='KRIGING', color="purple")
-#plt.plot(time, antilope, label='ANTILOPE', color="red")
-plt.plot(rs27.time, rs27, label='AS-ANTILOPE', color="blue")
-plt.plot(rs27_bis.time, rs27_bis, label='AS-ANTILOPE LPN+200m', color="green")
+
+if plot_lpn:
+    plt.plot(rs27.time, rs27, label='AS-ANTILOPE', color="blue")
+    plt.plot(rs27_bis.time, rs27_bis, label='AS-ANTILOPE LPN+200m', color="green")
+else:
+    plt.plot(safran.time, safran, label='SAFRAN', color="#1f78b4")
+    plt.plot(kriging.time, kriging, label='KRIGING', color="#e31a1c")
+    plt.plot(antilope.time, antilope, label='ANTILOPE', color="#b2df8a")
+    plt.plot(rs27.time, rs27, label='AS-ANTILOPE', color="#33a02c")
+
 deb = pd.Timestamp(year=int(datebegin[0:4]), month=int(datebegin[4:6]), day=int(datebegin[6:8]),  tz="UTC")
 end = pd.Timestamp(year=int(dateend[0:4]), month=int(dateend[4:6]), day=int(dateend[6:8]),  tz="UTC")
 if os.path.exists('HTN.obs'):
@@ -119,10 +121,9 @@ if os.path.exists('HTN.obs'):
     htn = htn[(htn.dat >= deb) & (htn.dat <= end)]
     ax.plot(htn.dat.values, htn.neigetot.values / 100, color='k', label='In-situ observation')
 
-    alt = htn.alt.unique()[0]
-
-    plot_lpn = True
     if plot_lpn:
+        alt = htn.alt.unique()[0]
+        ax.set_ylim([-ymax, ymax])
         ax2 = ax.twinx()
         lpn = pd.read_csv('LPN.obs', sep=';', parse_dates=['dat'])
         lpn = lpn[(lpn.dat >= deb) & (lpn.dat <= end)]
@@ -141,12 +142,15 @@ if os.path.exists('HTN.obs'):
         ax2.plot(isot.time.data, isot.data - alt, marker='*', color='b', linestyle='', label='Simulation LPN')
         ax2.set_ylim([-1800, 1800])
         ax2.set_ylabel("Elevation difference with Huez elevation (m)")
+        ax2.legend(loc='lower right')
+        diff = lpn.lpn.values - isot.data
+        pos = np.where(diff > 0)
+        neg = np.where(diff < 0)
+        ax2.vlines(lpn.dat.values[pos], isot.data[pos] - alt, lpn.lpn.values[pos] - alt, color='blue', linestyle='--', label='LPN under-estimation')
+        ax2.vlines(lpn.dat.values[neg], lpn.lpn.values[neg] - alt, isot.data[neg] - alt, color='red', linestyle='--', label='LPN over-estimation')
+    else:
+        ax.set_ylim([0, ymax])
 
-    diff = lpn.lpn.values - isot.data
-    pos = np.where(diff > 0)
-    neg = np.where(diff < 0)
-    ax2.vlines(lpn.dat.values[pos], isot.data[pos] - alt, lpn.lpn.values[pos] - alt, color='blue', linestyle='--', label='LPN under-estimation')
-    ax2.vlines(lpn.dat.values[neg], lpn.lpn.values[neg] - alt, isot.data[neg] - alt, color='red', linestyle='--', label='LPN over-estimation')
 
 if os.path.exists('HTN2.obs'):
     #obs = pd.read_csv('NIVOSE.obs', sep=';', parse_dates=['dat'])
@@ -169,6 +173,5 @@ for date in dates_pleiades:
 ax.set_ylabel('Snow depth (m)')
 ax.legend(loc='upper left')
 #ax.legend(loc='upper left', bbox_to_anchor=(0.05, 0.3, 0.5, 0.5))
-ax2.legend(loc='lower right')
 plt.tight_layout()
 plt.savefig('Chrono.pdf')
