e2b3bc02e8802a0e5d7d80f85aec38904251f9af
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2025-06-11 10:16:21 +0200

    Improve script to plot field from local file

--- scripts/plot_field.py
+++ scripts/plot_field.py
@@ -3,73 +3,82 @@
 # Auteur: Matthieu Vernay
 # Date : 05/11/2022
 
-import os, sys
-from datetime import datetime,timedelta
-import numpy as np
 import xarray as xr
-import rioxarray
-
 import argparse
 
-#import matplotlib
-#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from matplotlib.text import Annotation
+import cartopy.crs as ccrs
 
 from snowtools.plots.maps import plot2D
-from snowtools.scripts.extract.vortex import vortexIO as io
-import snowtools.tools.xarray_preprocess as xrp
+from snowtools.tools.xarray_backend import CENBackendEntrypoint
 
-###########################################
-# USAGE : p plot_field.py $filename [cumul]
-###########################################
+from vortex import toolbox
 
-latmax = 46.45
-latmin = 44.1
-lonmin = 5.4
-lonmax = 7.2
-
-filename = sys.argv[1]
-field = xr.open_dataarray(filename)
-field = xrp.preprocess(field)
-field = field.where((field.lon >= lonmin) & (field.lon <= lonmax) & (field.lat <= latmax) & (field.lat >= latmin), drop=True)
-field = field.rio.write_crs('EPSG:4326')
-
-# High-resolution (25m) DEM for fancy figures (set shade=True in plot_field calls)
-io.get_const('uenv:dem.1@vernaym', 'relief', 'alp', filename='TARGET_RELIEF.tif',
-    gvar='DEM_FRANCE25M_L93')
-dem = rioxarray.open_rasterio('TARGET_RELIEF.tif')
-dem = xrp.preprocess(dem)
-dem = dem.squeeze()
-dem = dem.rename({'xx': 'x', 'yy': 'y'})
-dem = dem.rio.reproject_match(field.rename({'lon': 'x', 'lat': 'y'}))
-dem = dem.rename({'x': 'xx', 'y': 'yy'})
+toolbox.active_now = True
 
 
-cumul = False
-figname = filename.replace('.nc', '.pdf')
-if len(sys.argv)>2:
-    cumul=True
-    figname = f"CUMUL_{figname}"
-    if 'member' in field.dims:
-        field = field.sum(dim=['time']).mean(dim=['member'])
+def parse_command_line():
+    description = "Plot 2D field"
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument('-f', '--filename', type=str, nargs='+', required=True,
+                        help="Name(s) of the files to plot")
+
+    parser.add_argument('-c', '--cumul', action='store_true',
+                        help='Plot accumulation over time')
+
+    parser.add_argument('-m', '--mean', action='store_true',
+                        help='Plot ensemble mean')
+
+    parser.add_argument('--vmin', type=float,
+                        help='Min colorscale value')
+
+    parser.add_argument('--vmax', type=float,
+                        help='Max colorscale value')
+
+    parser.add_argument('--cmap', default='YlGnBu',
+                        help='Colormap')
+
+    parser.add_argument('--label', nargs='*', default=None, help='Label of the field to plot')
+
+    parser.add_argument('--coordinates', nargs=4, type=float, default=None,
+                        help='list of min/max coordinates to extract, format:'
+                        '[latmax, latmin, lonmin, lonmax] (unit: °)')
+
+    args = parser.parse_args()
+    return args
+
+
+args = parse_command_line()
+for filename in args.filename:
+    field = xr.open_dataarray(filename, engine='cen')
+    field = field.rio.write_crs('EPSG:4326')
+    figname = filename.replace('.nc', '.pdf')
+
+    if args.cumul and 'time' in field.dims:
+        field = field.sum('time')
+    if args.mean:
+        field = field.mean('member')
+
+    if args.coordinates is not None:
+        latmax = args.coordinates[0]
+        latmin = args.coordinates[1]
+        lonmin = args.coordinates[2]
+        lonmax = args.coordinates[3]
+        field = field.where((field.xx >= lonmin) & (field.xx <= lonmax) & (field.yy >= latmin) &
+                (field.yy <= latmax), drop=True)
+        fig, ax = plt.subplots(figsize=(8 * len(field.xx) / len(field.yy), 6),
+                subplot_kw=dict(projection=ccrs.PlateCarree()), layout='tight')
+        ax.set_extent([lonmin, lonmax, latmin, latmax], crs=ccrs.PlateCarree())
     else:
-        field = field.sum(dim=['time'])
+        fig, ax = plt.subplots(subplot_kw=dict(projection=ccrs.PlateCarree()), layout='tight')
 
+    field = field.where(~field.isnull(), drop=True)
+    if args.label:
+        label = ' '.join(args.label)
+        field = field.rename(label)
 
-#fig, ax = plt.subplots(figsize=(12,6))
-fig, ax = plt.subplots(figsize=(14,16))
-#field.plot(ax=ax, cbar_kwargs={"label":'Total precipitation between {0:s} and {1:s} (mm)'.format(args.datebegin.strftime('%Y%m%d'), args.dateend.strftime('%Y%m%d'))}, cmap=plt.cm.coolwarm)
-#field.plot(ax=ax, cmap=plt.cm.Greys)
-#field.plot(ax=ax, cmap=plt.cm.YlGnBu)
-plot2D.plot_field(field, ax=ax, cmap=plt.cm.YlGnBu, dem=dem, shade=False, vmin=200, vmax=1200)
-
-#add_landmarks()
-#add_radar_positions(ax)
-#add_scores()
-plt.show()
-fig.savefig(figname, format='pdf', bbox_inches='tight')
-
-
-
-
+    plot2D.plot_field(field, ax=ax, cmap=args.cmap, vmin=args.vmin, vmax=args.vmax, boundaries=True, cities=True,
+            massifs=True)
+    ax.set_title("")
+    plot2D.save_fig(figname, fig=fig)
